cb21761c317c889201bbf1742c9ec8c8bb4c98ff
Stephanie Fernández <110364490+stephaniefdz@users.noreply.github.com>
2022-09-09 20:47:27 -0300

    Entrega 2 Arreglada Funciona OK

--- js/products.js
+++ js/products.js
@@ -1,84 +1,78 @@
 
 // Defino las constantes
+const GET_PRODUCTOS = PRODUCTS_URL;  // Obtengo la URL directamente del Init (Codigo en init modificado)  
 const listaProductos = document.getElementById("verProductos");
 const ORDER_ASC_BY_COST = "costoAscendente";
 const ORDER_DESC_BY_COST = "costoDescendente";
 const ORDER_BY_PROD_REL = "Relevancia";
-// const ORDER_BY_SEARCH = "Según Busqueda"; // Estoy probando si funciona
-const PRODUCTOS = PRODUCTS_URL + localStorage.catID + ".json";
+const ORDER_BY_SEARCH = "Según Busqueda"; 
+// const PRODUCTOS = PRODUCTS_URL + localStorage.catID + ".json";
 let productsArray =  [];
-let currentProductsArray = productsArray.product;
-let currentSortCriterio = undefined;
+let currentSortCriteria = undefined;
 let minCost = undefined;
 let maxCost = undefined;
 
 // Criterios de orden de productos
 
-function sortProducts(criterio, array) {
-    let result = [];
-    if (criterio === ORDER_ASC_BY_COST) {
-      result = array.sort(function (a, b) {
-        if (a.cost < b.cost) {
-          return -1;
-        }
-        if (a.cost > b.cost) {
-          return 1;
-        }
-        return 0;
+// Criterios de orden de productos
+
+function sortProducts(criteria, array){
+  let result = [];
+  if (criteria === ORDER_ASC_BY_COST)
+  {
+      result = array.sort(function(a, b) {
+          if ( a.cost < b.cost ){ return -1; }
+          if ( a.cost > b.cost ){ return 1; }
+          return 0;
       });
-    } else if (criterio === ORDER_DESC_BY_COST) {
-      result = array.sort(function (a, b) {
-        if (a.cost > b.cost) {
-          return -1;
-        }
-        if (a.cost < b.cost) {
-          return 1;
-        }
-        return 0;
+  }else if (criteria === ORDER_DESC_BY_COST){
+      result = array.sort(function(a, b) {
+          if ( a.cost > b.cost ){ return -1; }
+          if ( a.cost < b.cost ){ return 1; }
+          return 0;
       });
-    } else if (criterio === ORDER_BY_PROD_REL) {
-      result = array.sort(function (a, b) {
-        let aSold = parseInt(a.soldCount);
-        let bSold = parseInt(b.soldCount);
-  
-        if (aSold > bSold) {
-          return -1;
-        }
-        if (aSold < bSold) {
-          return 1;
-        }
-        return 0;
+  }else if (criteria === ORDER_BY_PROD_REL){
+      result = array.sort(function(a, b) {
+          let aCount = parseInt(a.soldCount);
+          let bCount = parseInt(b.soldCount);
+
+          if ( aCount > bCount ){ return -1; }
+          if ( aCount < bCount ){ return 1; }
+          return 0;
       });
-    }
-  
-    return result;
   }
+
+  return result;
+}
+  }
+
+
 function verListado() {
   let htmlContentToAppend = "";
-  for (let i = 0; i < currentProductsArray.length; i++) {
-    let product = currentProductsArray[i];
-    // let searchProduct = document.getElementById("searchItem").value.toLowerCase(); 
+  for (let i = 0; i < product.products.length; i++) {
+    let productos = product.products[i]; // products: Propiedad de la resp json 
+    let searchProduct = document.getElementById("searchItem").value.toLowerCase(); 
 
     // En el caso de que mo se selecione un MAX O MIN se deberian ver todos los productos
-    if ((minCost == undefined || (minCost != undefined && parseInt(product.cost) >= minCost)) &&
-      (maxCost == undefined || (maxCost != undefined && parseInt(product.cost) <= maxCost)) 
+    if ((minCost == undefined || (minCost != undefined && parseInt(productos.cost) >= minCost)) &&
+      (maxCost == undefined || (maxCost != undefined && parseInt(productos.cost) <= maxCost)&& productos.name.toLowerCase().includes(searchProduct)) 
       
     )
-    //  && product.name.toLowerCase().includes(searchProduct)
+    //  
     {
       htmlContentToAppend += `<div class="list-group" id="cat-list-container">   
                 <div class="bg-dark text-white shadow-none p-3 mb-5 bg-light rounded list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src= "${product.image}" alt="product image" class="img-fluid rounded"">
+                        <img src= "${productos.image}" alt="product image" class="img-fluid rounded"">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
                             <div class="mb-1">
-                            <h4>${product.name} - ${product.currency} : ${product.cost}</h4> 
-                            <p> ${product.description}</p> 
+                            <h4>${productos.name} - ${productos.currency} : ${productos.cost}</h4> 
+                            <p> ${productos.description}</p> 
                             </div>
-                            <small class="text-muted">${product.soldCount} Vendidos </small> 
+                            <small class="text-muted">${productos.soldCount} Vendidos </small> 
                         </div>
                     </div>
                     </div>    
@@ -89,43 +83,39 @@ function verListado() {
   listaProductos.innerHTML = htmlContentToAppend;
 }
 
-// document.addEventListener("DOMContentLoaded", function(e) {
-//     let resultArray = [];
-//     getJSONData(PRODUCTOS).then(function (resultObj) {
-//         if (resultObj.status === "ok") {
-//             resultArray = resultObj.data.products;
-//         }
-//         console.log(resultArray);
-//         verListado(resultArray);
-//     }
-//     )
-// });
-
-
 
 // Con esto se ordena y se muestran los productos ordenados según el criterio seleccionado
 
-function sortAndShowProducts(sortCriterio, productsArray) {
-  currentSortCriterio = sortCriterio;
+function sortAndShowProducts(sortCriteria, productsArray) {
+  currentSortCriteria = sortCriteria;
 
   if (productsArray != undefined) {
-    currentProductsArray = productsArray;
+    products = productsArray;
   }
 
-  currentProductsArray = sortProducts(currentSortCriterio,currentProductsArray);
+  sortProducts(currentSortCriteria, product.products);
 
   verListado();
 }
 
-document.addEventListener("DOMContentLoaded", function (e) {
-  getJSONData(PRODUCTOS).then(function (resultObj) {
-    if (resultObj.status === "ok") {
-        currentProductsArray = resultObj.data.product
-        verListado();  
-    //   let objeto = resultObj.data;
-    //   return objeto;
-    }
-  });
+
+//Obtengo los datos 
+
+document.addEventListener("DOMContentLoaded", async function() { // Trae la resp de la URL de CATID y se convierte en una variable PRODUCTS
+  getJSONData(GET_PRODUCTOS).then(function (response) {
+      if (response.status === "ok") {
+          product = response.data; 
+          console.log(product)
+          verListado();
+      }
+      console.log(CATID)  
+      
+     
+  }
+  )
+});
+
+// INICIO DE FUNCIONES DE ORDEN, FILTRADO 
 
   // Al dar click deberia ordenar de forma ascendente segun precio/costo 
   document.getElementById("sortAsc").addEventListener("click", function(){
@@ -160,22 +150,23 @@ document.getElementById("rangeFilterCost").addEventListener("click", function(){
 
 
     // Aca se verifica si minCost y maxCost son undefined / VACIOS para filtrarlos por su costo/precio
+   
     if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0){
-        minCost = parseInt(minCost);
-    }
-    else{
-        minCost = undefined;
-    }
+      minCost = parseInt(minCost);
+  }
 
-    if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
-        maxCost = parseInt(maxCost);
-    }
-    else{
-        maxCost = undefined;
-    }
+  else{
+      minCost = undefined;
+  }
 
+  if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
+      maxCost = parseInt(maxCost);
+  }
+
+  else{
+      maxCost = undefined;
+  }
+console.log(maxCost)
     verListado();
-});
-
 });
 

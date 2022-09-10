2d271b058a6af20a9bc948655a5706ba9d74f398
Stephanie Fernández <110364490+stephaniefdz@users.noreply.github.com>
2022-09-09 21:04:55 -0300

    Cambie estilo del cat-list-container

--- js/categories.js
+++ js/categories.js
@@ -49,22 +49,22 @@ function showCategoriesList(){
         if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
             ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
 
-            htmlContentToAppend += `
-            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${category.name}</h4>
-                            <small class="text-muted">${category.productCount} artículos</small>
+                htmlContentToAppend += `
+                <div onclick="setCatID(${category.id})" class="bg-dark text-white shadow-none p-3 mb-5 bg-light rounded list-group-item list-group-item-action" cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="${category.imgSrc}" alt="${category.description}" class="img-fluid rounded">
                         </div>
-                        <p class="mb-1">${category.description}</p>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">${category.name}</h4>
+                                <small class="text-muted">${category.productCount} artículos</small>
+                            </div>
+                            <p class="mb-1">${category.description}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            `
+                `
         }
 
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;

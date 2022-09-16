5ced7058ddd39edc41b99572fef6b1f0b557cd46
Stephanie Fernández <110364490+stephaniefdz@users.noreply.github.com>
2022-09-16 01:33:16 -0300

    Entrega 3 OK Pequeñas modificaciones  de tipeo y graficas

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 const CATID = localStorage.getItem("catID"); //
-const PRODID = localStorage.getItem("prodID"); 
+const prodID = localStorage.getItem("ID"); 
 const EXT_TYPE = ".json";
 const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
 const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
 const PRODUCTS_URL = `https://japceibal.github.io/emercado-api/cats_products/${CATID}${EXT_TYPE}`;
-const PRODUCT_INFO_URL = `https://japceibal.github.io/emercado-api/products/${PRODID}${EXT_TYPE}`;
-const PRODUCT_INFO_COMMENTS_URL = `https://japceibal.github.io/emercado-api/products_comments/${PRODID}${EXT_TYPE}`;
+const PRODUCT_INFO_URL = `https://japceibal.github.io/emercado-api/products/${prodID}${EXT_TYPE}`;
+const PRODUCT_INFO_COMMENTS_URL = `https://japceibal.github.io/emercado-api/products_comments/${prodID}${EXT_TYPE}`;
 const CART_INFO_URL =`https://japceibal.github.io/emercado-api/user_cart/${EXT_TYPE}`;
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 
@@ -49,5 +49,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
   let UserId = localStorage.getItem('mail');
   let AddLi = document.getElementsByTagName('li');
   let li = AddLi[3];
-  li.innerHTML = `<a class=text-primary nav-link">${UserId}</a>`;
+  li.innerHTML = `<a class=text-primary nav-link"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg> ${UserId}</a>`;
   });

72df98221af80b1c17589a06f7007c26651333bf
Stephanie Fernández <110364490+stephaniefdz@users.noreply.github.com>
2022-10-16 21:03:31 -0300

    E5 Actualización

--- js/cart.js
+++ js/cart.js
@@ -9,11 +9,11 @@ const productsinCartHTML = document.getElementById("cart");
     for (let i = 0; i < cart.length; i++) {
         showCartHtmlToAppend += ` 
         <tr> 
-            <th scope="col" class="${"art"+i} "><img src="${cart[i].image}" width="150px"></th>
-            <th scope="col" class="${"art"+i}">${cart[i].name}</th>
-            <th scope="col" class="${"art"+i}">${cart[i].currency} <span class="cost">${cart[i].unitCost}</span></th>
-            <th scope="col" class="${"art"+i}"><input type="number" min="1" max="5" value="${cart[i].count}" style="width: 70px;"></th>
-            <th scope="col" class="${"art"+i}">${cart[i].currency} <span class="subTotal" data-currency="${cart[i].currency}">${cart[i].unitCost * cart[i].count}</span></th>
+            <th scope="col"><img src="${cart[i].image}" width="150px"></th>
+            <th scope="col">${cart[i].name}</th>
+            <th scope="col">${cart[i].currency} <span class="cost">${cart[i].unitCost}</span></th>
+            <th scope="col"><input type="number" min="1" max="5" value="${cart[i].count}" style="width: 70px;"></th>
+            <th scope="col">${cart[i].currency} <span class="subTotal" data-currency="${cart[i].currency}">${cart[i].unitCost * cart[i].count}</span></th>
             <td> <button type="button" class="btn btn-link" onclick="removeArticle(0);"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#750000" class="bi bi-trash" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>

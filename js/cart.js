8f7e4d35bab4003f31da76505b5d9f2eba185abd
Stephanie Fernández <110364490+stephaniefdz@users.noreply.github.com>
2022-10-17 21:18:10 -0300

    E5 Final funciona todo OK
    Las funciones funcionan correctamente

--- js/cart.js
+++ js/cart.js
@@ -1,43 +1,105 @@
 
-const GET_CART = CART_INFO_URL; 
+
+// Defino las constantes necesarias 
+
+const GET_CART = CART_INFO_URL; // URL QUE CONTIENE LOS DATOS QUE QUIERO MOSTRAR 
 const productsinCartHTML = document.getElementById("cart"); 
 
-// Funcion que trae al articulo del usuario en el carrito 
 
-  function showCart(cart) {
-    showCartHtmlToAppend = '';
-    for (let i = 0; i < cart.length; i++) {
-        showCartHtmlToAppend += ` 
-        <tr> 
-            <th scope="col" class="${"art"+i} "><img src="${cart[i].image}" width="150px"></th>
-            <th scope="col" class="${"art"+i}">${cart[i].name}</th>
-            <th scope="col" class="${"art"+i}">${cart[i].currency} <span class="cost">${cart[i].unitCost}</span></th>
-            <th scope="col" class="${"art"+i}"><input type="number" min="1" max="5" value="${cart[i].count}" style="width: 70px;"></th>
-            <th scope="col" class="${"art"+i}">${cart[i].currency} <span class="subTotal" data-currency="${cart[i].currency}">${cart[i].unitCost * cart[i].count}</span></th>
-            <td> <button type="button" class="btn btn-link" onclick="removeArticle(0);"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#750000" class="bi bi-trash" viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-          </svg> </button></td>
+const USD = 41; 
+let TIPO_ENVIO = { premium: 15, express: 7, standard: 5 };
+
+// Funcion que trae al articulo del usuario en el carrito con un for y lo insertamos en el html cart que es un tbody
+
+function showCart(cart) {
+    htmlContentToAppend = '';
+    for (let i = 0; i < cart.length; i++) { //recorre todos los productos del carrito
+        htmlContentToAppend += ` 
+        <tr onchange="modify(event)"> 
+            <th scope="col" class="${'article'+i}"><img src="${cart[i].image}" width="150px"></th>
+            <th scope="col" class="${'article'+i}">${cart[i].name}</th>
+            <th scope="col" class="${'article'+i}">${cart[i].currency} <span class="cost">${cart[i].unitCost}</span></th>
+            <th scope="col" class="${'article'+i}"><input type="number" min="1" value="${cart[i].count}" data-class="${'article'+i}"></th>
+            <th scope="col" class="${'article'+i}">${cart[i].currency} <span class="subTotal" data-currency="${cart[i].currency}" data-subtotal="${cart[i].unitCost * cart[i].count}">${cart[i].unitCost * cart[i].count}</span></th>
+            <td> <button class="btn btn-outline-danger p-1" onclick="removeArticle(0);"><i class="fa-solid fa-trash"></i></button>
             </tr>
         `
     }
-
-
-    productsinCartHTML.innerHTML = showCartHtmlToAppend;
+    productsinCartHTML.innerHTML = htmlContentToAppend;
+//llamamos y ejecutamos a la función resumenCompra()
+    resumenCompra();
 }
 
 
+//Funcion para eliminar un articulo del carrito que recible como parametro id 
 
-
-// Funcion para eliminar articulo del carrito FUNCIONAL 
+// Defino la variable article
 
 let article = [];
-function removeArticle (id) {
 
-    let article = document.getElementsByClassName("art" +id);
-    article[0].parentElement.remove(); 
-    window.setTimeout(function(){location.reload()},200);  // Despues de presionar el boton de la papelera (icono en rojo), se elimina y se reinicia el articulo en pantalla (SOLO PARA ESTA ENTREGA) 
+function removeArticle(id) { 
+    
+    let article = document.getElementsByClassName('article'+id);
+    article[0].parentElement.remove();
+    window.setTimeout('location.reload()', 300);  //Solo para mostrar que funciona el boton de eliminar - se eliminara en proximas entregas 
+   //llamamos y ejecutamos a la función resumenCompra()
+    resumenCompra();
 }
+
+//Funcion que suma el precio de los productos en el subtotal
+//dataset: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
+
+function modify(event) {
+    dataArti = document.getElementsByClassName(event.target.dataset.class);
+
+    // Actualiza subtotal
+    if (parseInt(dataArti[3].getElementsByTagName('input')[0].value) <= 0) {
+        dataArti[3].getElementsByTagName('input')[0].value = 1;
+    }
+    let subtotal = parseFloat(dataArti[2].getElementsByClassName('cost')[0].innerHTML) * parseInt(dataArti[3].getElementsByTagName('input')[0].value);
+    let elem = dataArti[4].getElementsByClassName('subTotal')[0];
+    elem.innerHTML = subtotal;
+    elem.dataset.subtotal = subtotal;
+    //llamamos y ejecutamos a la función resumenCompra()
+    resumenCompra();
+}
+
+
+// Funcion para mostrar y actualizar el monto del subtotal, gastos de envio y gestion, total dinamicamente
+
+function resumenCompra() {
+    let envio = document.querySelector('input[type=radio]:checked'); //se ontiene el valor del boton tipo radio
+
+    //se define el contador subtotal, gastosEnvio y total inicializado en 0.
+    let subtotal = 0; 
+    let gastosEnvio = 0;
+    let total = 0;
+
+    let subTotalArticles = document.getElementsByClassName('subTotal');
+    for (elem of subTotalArticles) {
+        if (elem.dataset.currency === 'USD') {
+            // console.log(elem.dataset.subtotal);
+            subtotal += USD * parseFloat(elem.dataset.subtotal);
+        } else {
+            subtotal += parseFloat(elem.dataset.subtotal);
+        }
+    }
+
+// RESUMEN DE COMPRAS
+
+    // Muestro el monto en el subtotal del resumen de compra 
+    document.getElementById('subtotal').innerHTML = subtotal;
+    
+        // Muestro el monto del subtotal multiplicado por los costes de envio segun elección en el resumen 
+    gastosEnvio = subtotal * TIPO_ENVIO[envio.id] / 100;
+    document.getElementById('envio').innerHTML = gastosEnvio;
+
+    //Muestro el monto total en el resumen de compras sumando subTotal y envio 
+    document.getElementById('total').innerHTML = subtotal + gastosEnvio;
+}
+
+
+// Petición web a URL del carrito de compras con el usuario 25801
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -48,13 +110,15 @@ document.addEventListener("DOMContentLoaded", async function() {
         if (cart.status === "ok") {
             articles = cart.data.articles;
             console.log(articles);
-            showCart(articles);
+            showCart(articles); 
         }
         
        
     }
     )
 });
+
+
 
 
 
